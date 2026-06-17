5afe85fda547bdcd6b74c59342d465b2d0d69c63
LORENZO GNUTZMANN DA SILVA <lorenzo_g_da-silva@estudante.sesisenai.org.br>
2026-06-17 09:08:53 -0300

    exercicios de arrays prontos

--- arrays/code.js
+++ arrays/code.js
@@ -1,7 +1,34 @@
+//desafio 1
+console.log("desafio 1");
+
 let comidas = [
     "Pizza", "Hamburger", "Lasanha", "Pastel", "Sorvete"
 ]
 
 for (let comida of comidas) {
     console.log(comida);
+}
+
+//desafio 2
+console.log("desafio 2");
+
+let numeros = [
+    "10", "20", "30", "40", "50"
+];
+
+for (let numero of numeros) {
+    console.log("Número encontrado: ", numero);
+}
+
+
+//desafio 3
+console.log("desafio FINAL");
+
+let jogos = [
+    "Devil May Cry 5", "Sekiro", "Valorant", "Elden ring", "Genshit impact"
+]
+
+console.log("Os games mais melhores:");
+for (let jogo of jogos) {
+    console.log(jogo);
 }
